945125092aad69e94903d439423252f0418d4be6
darkmian939 <cerikdamian@gmail.com>
2024-04-07 21:46:37 -0500

    feat(StackNavigation):add screens scheme: about, profile, tabs and settings

---
+++ Stack-Navegation/src/Presentation/screens/About/AboutScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const AboutScreen = () => {
+    return (
+        <View>
+            <Text>Tab02 Screen</Text>
+        </View>
+    )
+}
+
+export default AboutScreen;
